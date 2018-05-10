5f9525d41111bbd83688034eff11057a76cd0a1c
s1260070 <s1260070@u-aizu.ac.jp>
2018-05-10 18:26:03 +0900

    12A

---
+++ ITP1_1/12a.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int x,y;
+	scanf("%d%d",&x,&y);
+	if(x>y){
+		printf("a > b\n",y,x);
+}
+	else if(x<y){
+	printf("a < b\n");
+}
+	else if(x==y){
+	printf("a == b\n");
+}
+return 0;
+}
